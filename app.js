7f7da36ca137e2813b564b278bf69f92eeac16b1
Shawn Conrad <shawn_conrad@homedepot.com>
2015-11-28 17:56:02 -0500

    Lecture 22: Common Directives (Part 2)

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("mainController", ['$scope', '$filter', function($scope, $filter) {
 
-    $scope.handle = '';
+    $scope.alertClick = function() {
+        alert("You've clicked!");
+    }
 
-    $scope.lowercasehandle = function () {
-        return $filter('lowercase')($scope.handle);
-    };
-
-    $scope.characters = 5;
-
-    $scope.rules = [
-        {rulename: "Your handle must be 5 characters long."},
-        {rulename: "Your handle must be letters or numbers."},
-        {rulename: "These rules are awesome."}
-    ];
+    $scope.name = "Freshco Da Flowa"
 
 }]);
 
