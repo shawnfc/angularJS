62f1bb94f99531aadf71a38a683908232b9e6152
Shawn Conrad <shawn_conrad@homedepot.com>
2015-11-29 22:04:15 -0500

    Ready for Lecture #25.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var myApp = angular.module("myApp", []);
 
-myApp.controller("mainController", ['$scope', '$filter', function($scope, $filter) {
+myApp.controller("mainController", ['$scope', '$filter', '$http', function($scope, $filter, $http) {
 
     $scope.alertClick = function() {
         alert("You've clicked!");
-    }
+    };
 
     $scope.name = "Freshco Da Flowa"
 
