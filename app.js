9f121096d7ed6a01bce4ae1dee0b63a421dc8210
Shawn Conrad <shawn_conrad@homedepot.com>
2015-12-03 11:10:42 -0500

    Finished Lecture: 28. Ready to start Section 6 - Custom Services

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ["ngRoute"]);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+        .when('/second/:num', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+});
 
 myApp.controller("mainController", ['$scope', '$location', '$log', function($scope, $location, $log) {
 
-    $log.info($location.path());
+    $scope.name = "Main";
+
+}]);
+
+myApp.controller("secondController", ['$scope', '$location', '$log', '$routeParams', function($scope, $location, $log, $routeParams) {
+
+    $scope.num = $routeParams.num || 1;
 
 }]);
