d35757050508fc527a9ceae46cdad44d8be1e471
Shawn Conrad <shawn_conrad@homedepot.com>
2016-01-01 13:10:18 -0500

    On to the bonus lectures!

--- app.js
+++ app.js
@@ -26,27 +26,69 @@ myApp.service('nameService', function() {
 
 });
 
-myApp.controller("mainController", ['$scope', '$location', '$log', 'nameService', function($scope, $location, $log, nameService) {
+myApp.controller("mainController", ['$scope', '$log', function($scope, $log) {
 
-    $scope.name = nameService.name;
+    $scope.people = [
+        {
+            name: "Shawn Conrad",
+            address: "555 Main St.",
+            city: "New York",
+            state: "NY",
+            zip: "10010"
+        },
+        {
+            name: "Grace Conrad",
+            address: "356 Able St",
+            city: "New York",
+            state: "NY",
+            zip: "10010"
+        },
+        {
+            name: "Sidney Conrad",
+            address: "55 Moto Court",
+            city: "New York",
+            state: "NY",
+            zip: "10010"
+        }
+    ];
 
-    $scope.$watch('name', function() {
-        nameService.name = $scope.name;
-    });
-
-    $log.log(nameService.name);
-    $log.log(nameService.nameLength());
+    $scope.formattedAddress = function(person) {
+        return person.address + ", " + person.city + ", " + person.state + " " + person.zip;
+    };
 
 }]);
 
 myApp.controller("secondController", ['$scope', '$location', '$log', '$routeParams', 'nameService', function($scope, $location, $log, $routeParams, nameService) {
 
-    $scope.name = nameService.name;
 
-    $scope.$watch('name', function() {
-        nameService.name = $scope.name;
-    });
-
-    $scope.num = $routeParams.num || 1;
 
 }]);
+
+myApp.directive('searchResult', function() {
+    return {
+        restrict: 'AE',
+        templateUrl: 'directives/search-result.html',
+        replace: true,
+        scope: {
+            personObject: "=",
+            formattedAddressFunction: "&"
+        },
+        compile: function(elem, attrs) {
+
+            console.log('Compiling...');
+            //elem.removeAttr("class")
+            console.log(elem.html());
+
+            return {
+
+                post: function(scope, elements, attrs) {
+
+                    console.log(scope);
+                    console.log(elements);
+
+                }
+
+            }
+        }
+    }
+});
